042e6e3569dc76bbdad8f9ebf809ceb3ce56663f
gabwebdesign <gab.webdesign@gmail.com>
2025-01-06 23:37:08 -0600

    cambiar comando playwright

--- utils/scrapping.js
+++ utils/scrapping.js
@@ -1,31 +1,39 @@
 const { chromium } = require('playwright');
 
 const scrapping = async (url,cardSelector,titleSelector,priceSelector) => {
-  // Inicia el navegador
-  const browser = await chromium.launch({ headless: true }); 
-  const page = await browser.newPage();
+  try {
+    // Inicia el navegador
+    const browser = await chromium.launch({ 
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"] 
+    }); 
+    const page = await browser.newPage();
 
-  // Navega a la página
-  await page.goto(url);
+    // Navega a la página
+    await page.goto(url);
 
-  // Espera a que carguen los elementos de los tours
-  await page.waitForLoadState('networkidle');
-  await page.waitForSelector(cardSelector,{ timeout: 60000 });
+    // Espera a que carguen los elementos de los tours
+    await page.waitForLoadState('networkidle');
+    await page.waitForSelector(cardSelector, { timeout: 60000 });
 
-  // Extrae los títulos y precios de los elementos
-  const tours = await page.$$eval(cardSelector, (cards, selectors) => {
-    return cards.map(card => {
-      const title = card.querySelector(selectors.titleSelector)?.innerText.trim();
-      const price = card.querySelector(selectors.priceSelector)?.innerText.trim();
-      return { title, price };
-    });
-  }, { titleSelector, priceSelector });
+    // Extrae los títulos y precios de los elementos
+    const tours = await page.$$eval(cardSelector, (cards, selectors) => {
+      return cards.map(card => {
+        const title = card.querySelector(selectors.titleSelector)?.innerText.trim();
+        const price = card.querySelector(selectors.priceSelector)?.innerText.trim();
+        return { title, price };
+      });
+    }, { titleSelector, priceSelector });
 
-  // Muestra los resultados
-  //console.log(tours);
+    // Muestra los resultados
+    console.log(tours);
 
-  // Cierra el navegador
-  await browser.close();
+    // Cierra el navegador
+    await browser.close();
+    return tours;
+  } catch (error) {
+    console.error('Error during scrapping:', error);
+  }
 }
 
 module.exports = scrapping;
