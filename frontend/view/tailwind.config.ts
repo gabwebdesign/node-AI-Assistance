4eac3ed7cd04304af23fcb43dc0d42e243f09240
gabwebdesign <gab.webdesign@gmail.com>
2025-01-06 22:04:40 -0600

    no servira

--- frontend/view/tailwind.config.ts
+++ frontend/view/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
